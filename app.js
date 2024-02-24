bb911b0b5393f899cdb2c5e745dd4bdb08b26cc1
elshora97 <A.elshora@dataserve.com.sa>
2024-02-25 00:47:05 +0200

    Added routes & controllers

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks.js");
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server is listening to port ${port}...`);
+});
---
+++ controllers/tasks.js
@@ -0,0 +1,20 @@
+const getAllTasks = (req, res) => {
+  res.send("all items");
+};
+
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.send("updateTask");
+};
+const deleteTask = (req, res) => {
+  res.send("deleteTask");
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
