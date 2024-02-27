1149fd97b1194924e42754f0c737dafe1e4879f1
elshora97 <A.elshora@dataserve.com.sa>
2024-02-27 13:12:43 +0200

    Added Database Connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks.js");
+const connectDB = require("./db/connect.js");
+require("dotenv").config();
 
 //middleware
 app.use(express.json());
@@ -13,6 +15,16 @@ app.get("/", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`server is listening to port ${port}...`);
-});
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server is listening to port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
